ee22b87ad4991422ece84ffd5acacb3201bc2d0e
jhastine <qjrsyumo@tip.edu.ph>
2025-11-09 13:41:13 +0800

    updated port for email sending for forgot password

--- heroes-backend/routes/users.js
+++ heroes-backend/routes/users.js
@@ -14,11 +14,14 @@ const RETRY_DELAY_MS = 100;
 
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST, 
-  port: process.env.SMTP_PORT || 2525,
-  secure: false, 
+  port: parseInt(process.env.SMTP_PORT) || 587, 
+  secure: process.env.SMTP_PORT == '465',
   auth: {
     user: process.env.SMTP_USER, 
     pass: process.env.SMTP_PASS 
+  },
+  tls: {
+    rejectUnauthorized: true
   }
 });
 

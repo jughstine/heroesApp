b5b362b56d6b8484a3a139cbb753d74e2375cff7
jhastine <qjrsyumo@tip.edu.ph>
2025-11-10 22:16:30 +0800

    Update users.js

--- heroes-backend/routes/users.js
+++ heroes-backend/routes/users.js
@@ -18,7 +18,7 @@ const transporter = nodemailer.createTransport({
   secure: process.env.SMTP_PORT == '465',
   auth: {
     user: process.env.SMTP_USER, 
-    pass: process.env.SMTP_PASS 
+    pass: process.env.SMTP_PASSWORD 
   },
   tls: {
     rejectUnauthorized: true
